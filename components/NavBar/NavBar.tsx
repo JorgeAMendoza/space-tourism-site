47e3a141bd15ed6fe5e2b7083209e5b7799972e5
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-09-04 08:38:53 -0500

    remove all backgrounds from buttons, adjust the container size, and style the navbar for mobile view

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,9 +3,20 @@ import Image from 'next/image'
 import logo from '@/public/images/icons/logo.svg'
 import Link from 'next/link'
 import menuIcon from '@/public/images/icons/icon-hamburger.svg'
+import closeIcon from '@/public/images/icons/icon-close.svg'
+import style from './navbar.module.css'
+import { barlowCondensed } from '@/app/font'
+import { useState } from 'react'
+
 const NavBar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+  // use effect to set to false when we go to a bigger screen
+
+  // use effect to apply overflow class depending on showNav
+
   return (
-    <div>
+    <div className={style.navbar}>
       <Link href="/" aria-label="link to homepage">
         <Image
           src={logo as string}
@@ -15,28 +26,51 @@ const NavBar = () => {
         />
       </Link>
 
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">00 Home</Link>
-            </li>
-            <li>
-              <Link href="/destinations">01 Destinations</Link>
-            </li>
-            <li>
-              <Link href="/crew">02 Crew</Link>
-            </li>
-            <li>
-              <Link href="technology">03Technology</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <button aria-label="click to toggle the menu" type="button">
-          <Image src={menuIcon as string} alt="menu icon" />
+      <nav
+        aria-label="navigation for space tourism website"
+        aria-hidden={showNav ? 'false' : 'true'}
+        id="nav-menu"
+      >
+        <button
+          type="button"
+          aria-label="click to close the nav menu"
+          onClick={() => setShowNav(false)}
+          aria-controls="nav-menu"
+        >
+          <Image src={closeIcon as string} alt="" />
         </button>
-      </div>
+        <ul className={barlowCondensed.className}>
+          <li>
+            <Link href="/">
+              <span>00</span> <span>Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/destinations">
+              <span>01</span> <span>Destination</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/crew">
+              <span>02</span> <span>Crew</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="technology">
+              <span>03</span> <span>Technology</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <button
+        aria-label="click to open the menu"
+        type="button"
+        aria-controls="nav-menu"
+        onClick={() => setShowNav(true)}
+      >
+        <Image src={menuIcon as string} alt="menu icon" />
+      </button>
     </div>
   )
 }
