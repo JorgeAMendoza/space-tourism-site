a4ec7c334c5f2d770325ea7eb5d5c5aba37e0705
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-09-07 06:17:56 -0500

    created a new nav bar element for tablet viewa and up, reapplied the styles and ensured they matched with the figma design

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -26,64 +26,96 @@ const NavBar = () => {
 
   return (
     <div className={style.navbar}>
-      <Link href="/" aria-label="link to homepage">
-        <Image
-          src={logo as string}
-          width={40}
-          height={40}
-          alt="space tourism logo"
-          className={style.logo}
-        />
-      </Link>
+      <div className={style.navContainer}>
+        <Link href="/" aria-label="link to homepage">
+          <Image
+            src={logo as string}
+            width={40}
+            height={40}
+            alt="space tourism logo"
+            className={style.logo}
+          />
+        </Link>
 
-      <nav
-        aria-label="navigation for space tourism website"
-        aria-hidden={showNav ? 'false' : 'true'}
-        id="nav-menu-mobile"
-        className={style.navMenuMobile}
-      >
-        <button
-          type="button"
-          aria-label="click to close the nav menu"
-          onClick={() => setShowNav(false)}
-          aria-controls="nav-menu-mobile"
+        <nav
+          aria-label="navigation for space tourism website"
+          aria-hidden={showNav ? 'false' : 'true'}
+          id="nav-menu-mobile"
+          className={style.navMenuMobile}
         >
-          <Image src={closeIcon as string} alt="" />
-        </button>
-        <ul className={barlowCondensed.className}>
-          <li data-active={pathname === '/'}>
-            <Link href="/">
-              <span>00</span> <span>Home</span>
-            </Link>
-          </li>
-          <li data-active={pathname === '/destinations'}>
-            <Link href="/destinations">
-              <span>01</span> <span>Destination</span>
-            </Link>
-          </li>
-          <li data-active={pathname === '/crew'}>
-            <Link href="/crew">
-              <span>02</span> <span>Crew</span>
-            </Link>
-          </li>
-          <li data-active={pathname === '/technology'}>
-            <Link href="technology">
-              <span>03</span> <span>Technology</span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+          <button
+            type="button"
+            aria-label="click to close the nav menu"
+            onClick={() => setShowNav(false)}
+            aria-controls="nav-menu-mobile"
+          >
+            <Image src={closeIcon as string} alt="" />
+          </button>
+          <ul className={barlowCondensed.className}>
+            <li data-active={pathname === '/'}>
+              <Link href="/">
+                <span>00</span> <span>Home</span>
+              </Link>
+            </li>
+            <li data-active={pathname === '/destinations'}>
+              <Link href="/destinations">
+                <span>01</span> <span>Destination</span>
+              </Link>
+            </li>
+            <li data-active={pathname === '/crew'}>
+              <Link href="/crew">
+                <span>02</span> <span>Crew</span>
+              </Link>
+            </li>
+            <li data-active={pathname === '/technology'}>
+              <Link href="technology">
+                <span>03</span> <span>Technology</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-      <button
-        aria-label="click to open the menu"
-        type="button"
-        aria-controls="nav-menu-mobile"
-        onClick={() => setShowNav(true)}
-        aria-hidden={showNav ? 'true' : 'false'}
-        className={style.hamburgerMenu}
-      >
-        <Image src={menuIcon as string} alt="menu icon" />
-      </button>
+        <nav
+          aria-label="navigation for space toursim website"
+          aria-hidden={showNav ? 'false' : 'true'}
+          id="nav-menu"
+          className={style.navMenu}
+        >
+          <ul className={barlowCondensed.className}>
+            <li data-active={pathname === '/'}>
+              <Link href="/">
+                <span>00</span> <span>Home</span>
+              </Link>
+            </li>
+            <li data-active={pathname === '/destinations'}>
+              <Link href="/destinations">
+                <span>01</span> <span>Destination</span>
+              </Link>
+            </li>
+            <li data-active={pathname === '/crew'}>
+              <Link href="/crew">
+                <span>02</span> <span>Crew</span>
+              </Link>
+            </li>
+            <li data-active={pathname === '/technology'}>
+              <Link href="technology">
+                <span>03</span> <span>Technology</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <button
+          aria-label="click to open the menu"
+          type="button"
+          aria-controls="nav-menu-mobile"
+          onClick={() => setShowNav(true)}
+          aria-hidden={showNav ? 'true' : 'false'}
+          className={style.hamburgerMenu}
+        >
+          <Image src={menuIcon as string} alt="menu icon" />
+        </button>
+      </div>
     </div>
   )
 }
