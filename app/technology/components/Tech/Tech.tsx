f12d95542bc1c7f513cb2ed19bcea9ea1e538264
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-10-23 06:08:45 -0500

    style desktop view
    style the desktop view of the tech page, adjust html structure for tech
    info and css spacing for those elmeents.

--- app/technology/components/Tech/Tech.tsx
+++ app/technology/components/Tech/Tech.tsx
@@ -12,22 +12,33 @@ const Tech = ({ data, children }: TechProps) => {
   return (
     <div className={style.tech}>
       <div className={style.nav}>{children}</div>
-      <h2 className={style.title}>
-        <p>
-          the terminology<span>...</span>
-        </p>
-        {data.name}
-      </h2>
+      <div className={style.techInfo}>
+        <h2 className={style.title}>
+          <p>
+            the terminology<span>...</span>
+          </p>
+          {data.name}
+        </h2>
+        <p className={style.description}>{data.description}</p>
+      </div>
+
       <div className={style.image}>
         <Image
           src={data.images.landscape}
           alt={data.name}
           width={375}
           height={170}
+          className={style.mobileImage}
+        />
+
+        <Image
+          src={data.images.portrait}
+          alt={data.name}
+          width={515}
+          height={527}
+          className={style.portraitImage}
         />
       </div>
-
-      <p className={style.description}>{data.description}</p>
     </div>
   )
 }
