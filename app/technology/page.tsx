9d902f3bb3808a479795182cec65cc922ff3954a
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-10-24 11:42:26 -0500

    modify tech desktop structure.
    Separate the util container from the tech page container, giving it the
    `position:relative` value so that the image sticks to the edge of the
    container rather than the edge of the screen.

--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -1,6 +1,7 @@
 import { fetchTechnologyData } from '@/services/api/technology'
 import TechSlider from './components/TechSlider/TechSlider'
 import style from './technology.module.css'
+import util from '../../styles/utils.module.css'
 
 const getData = () => {
   const data = fetchTechnologyData()
@@ -10,15 +11,17 @@ const getData = () => {
 const TechnologyPage = () => {
   const data = getData()
   return (
-    <main className={style.technology}>
-      <h1 className={style.pageTitle}>
-        <p>03</p> space launch 101
-      </h1>
+    <div className={util.container}>
+      <main className={style.technology}>
+        <h1 className={style.pageTitle}>
+          <p>03</p> space launch 101
+        </h1>
 
-      <section>
-        <TechSlider data={data} />
-      </section>
-    </main>
+        <section>
+          <TechSlider data={data} />
+        </section>
+      </main>
+    </div>
   )
 }
 
